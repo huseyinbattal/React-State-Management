af0659f5b10600ce6ca9ab01512b4fb7663c08a8
huseyinbatta <huseyinbattal42@gmail.com>
2022-05-22 23:17:11 +0300

    position

--- src/Navi.js
+++ src/Navi.js
@@ -1,73 +1,73 @@
 import React, { Component } from 'react'
-import { Navbar, NavbarBrand, NavbarToggler,Collapse,NavItem,Nav,NavLink,UncontrolledDropdown,DropdownToggle,DropdownMenu,DropdownItem,NavbarText,Toast,ToastHeader} from 'reactstrap'
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Nav, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, Toast, ToastHeader } from 'reactstrap'
 
 
 
 export default class Navi extends Component {
   render() {
     return (
-<div>
-  <Navbar
-    color="light"
-    expand="md"
-    light
-  >
-    <NavbarBrand href="/">
-      <img width="50px" src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png'/>
-    </NavbarBrand>
-    <NavbarToggler onClick={function noRefCheck(){}} />
-    <Collapse navbar>
-      <Nav
-        className="me-auto"
-        navbar
-      >
-        <NavItem>
-          <NavLink href="#">
-            Components
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://github.com/huseyinbattal">
-            GitHub
-          </NavLink>
-        </NavItem>
-        <UncontrolledDropdown
-          inNavbar
-          nav
+      <div>
+        <Navbar
+          color="light"
+          expand="md"
+          light
         >
-          <DropdownToggle
-            caret
-            nav
-          >
-            Options
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>
-              Option 1
-            </DropdownItem>
-            <DropdownItem>
-              Option 2
-            </DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>
-              Reset
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
+          <NavbarBrand href="/">
+            <img width="50px" src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png' />
+          </NavbarBrand>
+          <NavbarToggler onClick={function noRefCheck() { }} />
+          <Collapse navbar>
+            <Nav
+              className="me-auto"
+              navbar
+            >
+              <NavItem>
+                <NavLink href="#">
+                  Components
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/huseyinbattal">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown
+                inNavbar
+                nav
+              >
+                <DropdownToggle
+                  caret
+                  nav
+                >
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             </Nav>
-            
+
             <NavbarText>
-          
- 
+              <Toast style={{ width:"120px", marginTop: "10px",position:"fixed", top:"0",right:"0"}}>
+                <ToastHeader icon="success">
+                  {this.props.cart.length} Items Added.
+                </ToastHeader>
+              </Toast>
+
             </NavbarText>
-            <Toast style={{width:"180px", marginTop:"10px"}}>
-    <ToastHeader icon="success">
-    {this.props.cart.length} Items Added.
-    </ToastHeader>
-  </Toast>
-    </Collapse>
-  </Navbar>
-</div>
+
+          </Collapse>
+        </Navbar>
+      </div>
     )
   }
 }
